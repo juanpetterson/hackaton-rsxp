afb3adb0eb0e878d0714ef99f663d9decb041349
juanpetterson <juan.petterson@gmail.com>
2019-11-26 17:27:23 -0300

    Adicionadas rotas de dashboard e lessons

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 
 import Home from 'pages/Home';
-// import Dashboard from 'pages/Dashboard';
+import Dashboard from 'pages/Dashboard';
+import Lessons from 'pages/Lessons';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
-      {/* <Route path="/" exact component={Dashboard} /> */}
+      <Route path="/dashboard" exact component={Dashboard} />
+      <Route path="/lessons/:id" exact component={Lessons} />
       <Redirect from="*" to="/" />
     </Switch>
   </BrowserRouter>
