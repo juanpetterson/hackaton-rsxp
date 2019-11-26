37197e1ff931b33a23d5d9d68bf7f7d8b0a17048
joaobenthin <joaopaulobenthin@hotmail.com>
2019-11-26 19:02:17 -0300

    list events

---
+++ src/pages/Events/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const List = styled.ul`
+  width: 70%;
+  margin: 0;
+`;
+
+export const Event = styled.li`
+  width: 100%;
+  height: 128px;
+
+  display: flex;
+  align-items: center;
+  margin: 8px 16px;
+  padding: 0 10px;
+  font-size: 36px;
+  transition: 0.2s;
+
+  &:hover {
+    background: #d4d4d4;
+  }
+`;
+export const Ranking = styled.div`
+  width: 30%;
+`;
+
+export const Title = styled.div``;
+
+export const School = styled.div``;
+
+export const SchoolImage = styled.div``;
+
+export const SchoolWrapper = styled.div``;
+
+export const SchoolTitle = styled.div``;
+
+export const SchoolRating = styled.div``;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 
 import Home from 'pages/Home';
 // import Dashboard from 'pages/Dashboard';
+import Events from 'pages/Events';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
       {/* <Route path="/" exact component={Dashboard} /> */}
+      <Route path="/events" component={Events} />
       <Redirect from="*" to="/" />
     </Switch>
   </BrowserRouter>
