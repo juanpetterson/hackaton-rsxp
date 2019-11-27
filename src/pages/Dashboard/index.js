f402e2fe343eecd8b8b7ebda1d77c2f1ba08432d
Marcílio Bezerra <marciliocb@gmail.com>
2019-11-27 01:02:44 -0300

    Método que verifica a lição atual

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
+
+  const [currentLevel,setCurrentLevel] = useState(1);
+
+  function checkLevel(){
+    var maxLevels = 3;
+    for(let i = 0; i < maxLevels; i++){
+      const level = window.localStorage.getItem(`lesson_${i+1}`);
+      if(!level){
+        console.log(i,i+1);
+        setCurrentLevel(i+1);
+        break;
+      }
+    }
+  }
+
+  useEffect(()=>{
+    checkLevel();
+  })
+
   return (
     <div>
       <h1>Dashboard</h1>
-      <Link to="/lessons/1">Ir para a lição 1</Link>
+      <Link to={`/lessons/${currentLevel}`}>Ir para a lição {currentLevel}</Link>
     </div>
   );
 }
