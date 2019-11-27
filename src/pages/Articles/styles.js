11ede40611e50c89b6365be2a1e843f0036bc3ff
Marcílio Bezerra <marciliocb@gmail.com>
2019-11-27 11:27:18 -0300

    Ajustes gerais

--- src/pages/Articles/styles.js
+++ src/pages/Articles/styles.js
@@ -1,20 +1,21 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 600px;
+  /* max-width: 600px; */
   margin: 50px auto;
 `;
 
 export const Main = styled.div`
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   text-align: center;
   justify-content: center;
-  width: 800px;
+  width: 80%;
   border-radius: 4px;
   background: #ccc;
   padding: 50px;
-
+  box-sizing: border-box;
   h1 {
     margin-bottom: 10px;
   }
@@ -27,9 +28,9 @@ export const List = styled.ul`
   list-style: none;
   display: flex;
   flex-direction: row;
-
+  flex-wrap: wrap;
   justify-content: center;
-  width: 800px;
+  /* width: 800px; */
 
   padding: 50px;
 
