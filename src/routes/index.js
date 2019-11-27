12e5c070ead0c50cfbe35114508a9b7fb00a42ff
joaobenthin <joaopaulobenthin@hotmail.com>
2019-11-27 12:10:45 -0300

    browserroute

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Home from 'pages/Home';
 import Events from 'pages/Events';
 import Dashboard from 'pages/Dashboard';
 import Lessons from 'pages/Lessons';
 import Articles from 'pages/Articles';
-import Header from 'components/Header';
 
 const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/articles" component={Articles} />
-      <Route path="/events" component={Events} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/lessons/:id" component={Lessons} />
-      <Redirect from="*" to="/" />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/articles" component={Articles} />
+    <Route path="/events" component={Events} />
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/lessons/:id" component={Lessons} />
+    <Redirect from="*" to="/" />
+  </Switch>
 );
 
 export default Routes;
